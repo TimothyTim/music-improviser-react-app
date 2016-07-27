06bc93826bef04c629202a9d5443b71f4bb6d5ec
Tim Pritchard <tim.pritchard01@bbc.co.uk>
2016-07-27 13:00:05 +0100

    interval and scale

--- src/components/Rhythm/Model/Interval.js
+++ src/components/Rhythm/Model/Interval.js
@@ -1,15 +1,46 @@
 import Chromatic from './Chromatic';
 
+function filterInterval(interval) {
+  if (interval > 12) {
+    interval = 12;
+  } else if (interval < -12) {
+    interval = -12;
+  }
+
+  return interval;
+}
+
+function getNextNote(note, interval) {
+  const indexOfNote = Chromatic.indexOf(note.name);
+  let octaveOfNextNote = note.octave;
+  let indexOfNextNote = indexOfNote + interval;
+
+  if (indexOfNote === -1) {
+      console.error('Not a valid note');
+      return ;
+  }
+
+  while(indexOfNextNote >= 12) {
+    indexOfNextNote -= 12;
+    octaveOfNextNote += 1;
+  }
+
+  while(indexOfNextNote <= 12) {
+    indexOfNextNote += 12;
+    octaveOfNextNote -= 1;
+  }
+
+  return {
+    name: Chromatic[indexOfNextNote],
+    octave: octaveOfNextNote
+  };
+}
+
 export default {
-    getNote: (note, interval, octave) => {
-        const noteOctave = octave ? octave : 3;
-        const indexOfNote = Chromatic.indexOf(note);
+    getNote: (note, interval) => {
+        if (!note.octave) note.octave = 3;
+        interval = filterInterval(interval);
 
-        if (indexOfNote === -1) {
-            console.error('Not a valid note');
-            return ;
-        }
-
-        return Chromatic[indexOfNote + interval] + noteOctave;
+        return getNextNote(note, interval);
     }
 };
--- src/components/Rhythm/Model/Scale.js
+++ src/components/Rhythm/Model/Scale.js
@@ -1,16 +1,21 @@
-class Scale {
-    constructor(key) {
-        this.key = key;
-        this.octive = 4;
-    }
+import intvl from './Interval.js';
 
-    getChromatic() {
+let Scale = {};
 
-    }
+Scale.chromatic = (note) => {
+  let scaleByIntervals = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+  let scale = [];
+  if (!note.octave) note.octave = 3;
 
-    getPentatonic() {
-        
-    }
+  scaleByIntervals.forEach((interval) => {
+    scale.push(intvl.getNote(note, interval));
+  });
+
+  return scale;
+}
+
+Scale.pentatonic = () => {
+
 }
 
 export default Scale;
